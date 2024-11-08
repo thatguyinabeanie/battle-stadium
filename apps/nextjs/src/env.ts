1b5c88efabfe920e5e1d2761b0b4af58d9b34190
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-08 15:10:38 -0600

    no upload thing token

--- apps/nextjs/src/env.ts
+++ apps/nextjs/src/env.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 import { env as authEnv } from "@battle-stadium/auth/env";
 
 export const env = createEnv({
-  extends: [authEnv, vercel(), uploadthing()],
+  extends: [authEnv, vercel()],
   shared: {
     NODE_ENV: z
       .enum(["development", "production", "test"])
@@ -37,6 +37,7 @@ export const env = createEnv({
    * Destructure all variables from `process.env` to make sure they aren't tree-shaken away.
    */
   experimental__runtimeEnv: {
+
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
