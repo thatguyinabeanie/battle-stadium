944f4e55839d90c5e549a8671d1fe753e0c348f7
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-07 21:13:21 -0600

    yeet the cruft

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,12 +1,4 @@
-import { Suspense } from "react";
-
 import { HydrateClient } from "~/trpc/server";
-
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
 
 export default function HomePage () {
   // You can await this here if you don't want to show Suspense fallback below
@@ -19,22 +11,6 @@ export default function HomePage () {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Create <span className="text-primary">T3</span> Turbo
           </h1>
-
-
-          <CreatePostForm />
-          <div className="w-full max-w-2xl overflow-y-scroll">
-            <Suspense
-              fallback={
-                <div className="flex w-full flex-col gap-4">
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                  <PostCardSkeleton />
-                </div>
-              }
-            >
-              <PostList />
-            </Suspense>
-          </div>
         </div>
       </main>
     </HydrateClient>
