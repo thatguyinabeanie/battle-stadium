f8bc86360e5360b9546c0a1c00e6d0c0222cb8b4
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-08 15:53:29 -0600

    accounts router placeholder

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
+import { accountsRouter } from "./router/accounts";
 import { authRouter } from "./router/auth";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  accounts: accountsRouter,
 });
 
 // export type definition of API
