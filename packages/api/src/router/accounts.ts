eb5aa8a62a1d891668ec69886c177d7174539e89
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-08 15:53:43 -0600

    accounts router placeholder

---
+++ packages/api/src/router/accounts.ts
@@ -0,0 +1,19 @@
+import type { TRPCRouterRecord } from "@trpc/server";
+
+import { protectedProcedure, publicProcedure } from "../trpc";
+
+export const accountsRouter = {
+  getSession: publicProcedure.query(({ ctx }) => {
+    return ctx.session;
+  }),
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can see this secret message!";
+  }),
+  signOut: protectedProcedure.mutation((opts) => {
+    if (!opts.ctx.token) {
+      return { success: false };
+    }
+
+    return { success: true };
+  }),
+} satisfies TRPCRouterRecord;
