8910488a08face79f39453373a6fececbc40b848
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-07 21:35:27 -0600

    save

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -18,18 +18,18 @@ export const metadata: Metadata = {
       ? "https://battlestadium.gg"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
+  title: "Battle Stadium",
   description: "Simple monorepo with shared backend for web & mobile apps",
   openGraph: {
-    title: "Create T3 Turbo",
+    title: "Battle Stadium",
     description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    url: "https://battlestadium.gg",
+    siteName: "Battle Stadium",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
+    site: "@thatguyinabenaie",
+    creator: "@thatguyinabenaie",
   },
 };
 
