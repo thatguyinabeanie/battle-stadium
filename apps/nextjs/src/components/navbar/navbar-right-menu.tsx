7b9143893c929a54f3cd72ec0eb30029946a84d7
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-08 18:24:45 -0600

    save

--- apps/nextjs/src/components/navbar/navbar-right-menu.tsx
+++ apps/nextjs/src/components/navbar/navbar-right-menu.tsx
@@ -3,14 +3,6 @@ import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
-import {
-  Badge,
-  Button,
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@battle-stadium/ui";
-
 import { getAccountMe } from "~/app/server-actions/accounts/actions";
 import UserMenu from "./user-menu/user-menu";
 
@@ -19,7 +11,7 @@ export default async function RightMenu() {
   const me = (await getAccountMe())?.data;
 
   return (
-    <div>
+    <div className="flex flex-row items-center justify-center gap-2">
       <Button>
         <Icon
           className="text-default-500"
@@ -36,26 +28,9 @@ export default async function RightMenu() {
             width={24}
           />
         </Button>
-
-        <Popover>
-          <PopoverTrigger>
-            <Badge color="danger" content="5">
-              <Icon
-                className="text-default-500"
-                icon="solar:bell-linear"
-                width={22}
-              />
-            </Badge>
-          </PopoverTrigger>
-          <PopoverContent className="max-w-[90vw] p-0 sm:max-w-[380px]">
-            <p className="text-default-500 py-4 text-center">
-              See all notifications
-            </p>
-          </PopoverContent>
-        </Popover>
-
-        <UserMenu isSignedIn={!!clerkAuth.sessionId} me={me} />
       </Link>
+
+      <UserMenu isSignedIn={!!clerkAuth.sessionId} me={me} />
     </div>
   );
 }
