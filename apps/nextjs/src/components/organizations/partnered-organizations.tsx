47c70a67bae6373994a71b0f6e37d675c3c4ffc8
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-10 14:39:04 -0600

    fix some linting whining about stuff

--- apps/nextjs/src/components/organizations/partnered-organizations.tsx
+++ apps/nextjs/src/components/organizations/partnered-organizations.tsx
@@ -1,18 +1,18 @@
 import { getPartneredOrganizations } from "~/app/server-actions/organizations/actions";
 import OrganizationCard from "~/components/organizations/organization-card";
 
-export default async function PartneredOrganizations() {
+export default async function PartneredOrganizations () {
   const orgs = await getPartneredOrganizations();
 
   return (
     <div className="flex h-full w-full flex-row items-center justify-center bg-transparent">
-      {orgs.map((organization) => (
+      { orgs.map((organization) => (
         <OrganizationCard
-          key={organization.id}
-          aria-label={`organization-card-${organization.id}`}
-          organization={organization}
+          key={ organization.slug }
+          aria-label={ `organization-card-${organization.slug}` }
+          organization={ organization }
         />
-      ))}
+      )) }
     </div>
   );
 }
