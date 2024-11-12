40547df80a1584ee865611d42ea715c02c148e39
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-11 18:21:42 -0600

    fix paths

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -5,7 +5,9 @@ import { SpeedInsights as VercelSpeedInsights } from "@vercel/speed-insights/nex
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
-import { cn, ThemeProvider, ThemeToggle } from "@battle-stadium/ui";
+import { cn, ThemeProvider } from "@battle-stadium/ui";
+
+// import { cn, ThemeProvider, ThemeToggle } from "@battle-stadium/ui";
 
 import { env } from "~/env";
 import { TRPCReactProvider } from "~/trpc/react";
@@ -26,7 +28,9 @@ import { siteConfig } from "~/lib/config/site";
 import { UploadThingRouter } from "./api/uploadthing/core";
 
 const Cookies = dynamic(() => import("~/components/cookies/cookies"));
-const AwesomeParticles = dynamic(() => import("~/components/awesome-particles"));
+const AwesomeParticles = dynamic(
+  () => import("~/components/awesome-particles"),
+);
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -65,7 +69,7 @@ export const viewport: Viewport = {
   ],
 };
 
-export default async function RootLayout ({
+export default async function RootLayout({
   children,
 }: Readonly<ChildrenProps>) {
   const { userId, sessionId } = await auth();
@@ -75,16 +79,16 @@ export default async function RootLayout ({
       <ClerkProvider>
         <html lang="en" suppressHydrationWarning>
           <body
-            className={ cn(
+            className={cn(
               "min-h-screen overflow-y-scroll bg-background font-sans text-foreground antialiased",
               GeistSans.variable,
               GeistMono.variable,
-            ) }
+            )}
           >
             <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
               <TRPCReactProvider>
                 <NextSSRPlugin
-                  routerConfig={ extractRouterConfig(UploadThingRouter) }
+                  routerConfig={extractRouterConfig(UploadThingRouter)}
                 />
                 <div className="flex min-h-screen flex-col items-center">
                   <AwesomeParticles />
@@ -94,7 +98,7 @@ export default async function RootLayout ({
 
                     <main className="flex min-h-screen w-full flex-col items-center">
                       <section className="z-0 flex w-full flex-col items-center gap-4">
-                        { children }
+                        {children}
                       </section>
                     </main>
 
@@ -107,13 +111,13 @@ export default async function RootLayout ({
                 <ThemeToggle />
               </div> */}
 
-              <Cookies isSignedIn={ !!sessionId } userId={ userId } />
+              <Cookies isSignedIn={!!sessionId} userId={userId} />
 
               <VercelAnalytics />
 
-              { env.VERCEL_ENV === "production" && <VercelSpeedInsights /> }
+              {env.VERCEL_ENV === "production" && <VercelSpeedInsights />}
 
-              <GoogleAnalytics gaId={ env.MEASUREMENT_ID } />
+              <GoogleAnalytics gaId={env.MEASUREMENT_ID} />
             </ThemeProvider>
           </body>
         </html>
