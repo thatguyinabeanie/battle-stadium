146a2e0a91cb1c8f9559a96dc622ccf061ec99d2
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-12 18:56:26 -0600

    prettier

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: "battlestadium.gg",
 };
 
-export default function Home () {
+export default function Home() {
   return (
     <HydrateClient>
       <div className="min--h-screen flex flex-col items-center justify-between">
@@ -21,17 +21,17 @@ export default function Home () {
         <Separator className="w-full" />
 
         <div className="flex w-full max-w-fit flex-col items-center justify-center bg-transparent text-center">
-          <h1 className={ title({ color: "violet", size: "xl" }) }>
+          <h1 className={title({ color: "violet", size: "xl" })}>
             battlestadium.gg
           </h1>
-          <h2 className={ `${title({ size: "xs" })} pt-2` }>
+          <h2 className={`${title({ size: "xs" })} pt-2`}>
             a next-gen tournament website
           </h2>
           <div className="flex flex-col justify-items-center">
-            <h2 className={ `${title({ size: "xxs" })} pt-1` }>
+            <h2 className={`${title({ size: "xxs" })} pt-1`}>
               beautiful, fast, modern
             </h2>
-            <h2 className={ `${title({ color: "violet", size: "xs" })} pt-4` }>
+            <h2 className={`${title({ color: "violet", size: "xs" })} pt-4`}>
               Coming Soon
             </h2>
           </div>
--- apps/nextjs/src/components/organizations/partnered-organizations.tsx
+++ apps/nextjs/src/components/organizations/partnered-organizations.tsx
@@ -1,19 +1,20 @@
 import * as React from "react";
+
 import { getPartneredOrganizations } from "~/app/server-actions/organizations/actions";
 import OrganizationCard from "./organization-card";
 
-export default async function PartneredOrganizations () {
+export default async function PartneredOrganizations() {
   const orgs = await getPartneredOrganizations();
 
   return (
     <div className="flex h-full w-full flex-row items-center justify-center bg-transparent">
-      { orgs.map((organization) => (
+      {orgs.map((organization) => (
         <OrganizationCard
-          key={ organization.slug }
-          aria-label={ `organization-card-${organization.slug}` }
-          organization={ organization }
+          key={organization.slug}
+          aria-label={`organization-card-${organization.slug}`}
+          organization={organization}
         />
-      )) }
+      ))}
     </div>
   );
 }
