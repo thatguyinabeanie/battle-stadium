4f86673a699db6108e030e3ececaebd95e94c74d
Gabe Mendoza <thatguyin@beanie.gg>
2024-11-21 20:38:08 -0600

    Disable navbar hide functionality (#49)
    * disable navbar hide
    
    * lint fix

--- apps/nextjs/src/components/navbar/navbar-container.tsx
+++ apps/nextjs/src/components/navbar/navbar-container.tsx
@@ -6,16 +6,38 @@ import { cn } from "@battle-stadium/ui";
 
 interface NavbarContainerProps {
   children: React.ReactNode;
+  disableHide?: boolean; // Optional prop
 }
 
 export default function NavbarContainer({
   children,
+  disableHide = false,
 }: Readonly<NavbarContainerProps>) {
+  const { isVisible } = useNavbarVisibility(disableHide);
+
+  return (
+    <div
+      className={cn(
+        "sticky top-0 z-40 w-full transform backdrop-blur-3xl transition-transform duration-300",
+        !isVisible && "-translate-y-full",
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+function useNavbarVisibility(disableHide: boolean) {
   const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
     const controlNavbar = () => {
+      if (disableHide) {
+        setIsVisible(true);
+        return;
+      }
+
       const currentScrollY = window.scrollY;
       const scrollThreshold = 100;
 
@@ -32,16 +54,7 @@ export default function NavbarContainer({
 
     window.addEventListener("scroll", controlNavbar);
     return () => window.removeEventListener("scroll", controlNavbar);
-  }, [lastScrollY]);
+  }, [lastScrollY, disableHide]);
 
-  return (
-    <div
-      className={cn(
-        "sticky top-0 z-40 w-full transform backdrop-blur-3xl transition-transform duration-300",
-        !isVisible && "-translate-y-full",
-      )}
-    >
-      {children}
-    </div>
-  );
+  return { isVisible };
 }
