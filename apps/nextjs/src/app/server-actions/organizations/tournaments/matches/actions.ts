e0b2f173dd53869cde5cee649d8c8d683e3f2b1b
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-23 17:39:54 -0600

    pnpm fix

---
+++ apps/nextjs/src/app/server-actions/organizations/tournaments/matches/actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { and, db, desc, eq } from "@battle-stadium/db";
+import { matches, organizations, tournaments } from "@battle-stadium/db/schema";
+
+function MatchesLeftJoinTournamentsLeftJoinOrganizations() {
+  return db
+    .select()
+    .from(matches)
+    .leftJoin(tournaments, eq(matches.tournamentId, tournaments.id))
+    .leftJoin(organizations, eq(tournaments.organizationId, organizations.id));
+}
+
+export async function getOrganizationTournamentMatches(
+  org_slug: string,
+  tournament_id: number,
+  page = 1,
+  pageSize = 20,
+) {
+  "use cache";
+  return await MatchesLeftJoinTournamentsLeftJoinOrganizations()
+    .where(
+      and(eq(organizations.slug, org_slug), eq(tournaments.id, tournament_id)),
+    )
+    .orderBy(desc(matches.createdAt))
+    .limit(pageSize)
+    .offset((page - 1) * pageSize);
+}
