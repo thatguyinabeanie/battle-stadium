bd98476c519de19d3a78ae66b60932563e09f927
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-22 23:47:49 -0600

    not-found

--- apps/nextjs/src/app/organizations/[org_slug]/page.tsx
+++ apps/nextjs/src/app/organizations/[org_slug]/page.tsx
@@ -45,25 +45,24 @@ export default async function OrganizationDetailPage(
 
   if (!organization) {
     notFound();
-  } else {
-    return (
-      <>
-        <OrganizationHeader
-          organization={organization}
-          classNames={{ wrapper: "my-8" }}
-        >
-          <div className="mx-4 flex h-full w-full flex-col items-center justify-between py-2 text-center">
-            <h1 className="text-2xl font-semibold">{organization.name}</h1>
-            <p>{organization.description}</p>
-          </div>
-        </OrganizationHeader>
-
-        <SingleOrganizationTournamentsTable
-          className="w-full px-4"
-          data={tournaments}
-          organization={organization}
-        />
-      </>
-    );
   }
+  return (
+    <>
+      <OrganizationHeader
+        organization={organization}
+        classNames={{ wrapper: "my-8" }}
+      >
+        <div className="mx-4 flex h-full w-full flex-col items-center justify-between py-2 text-center">
+          <h1 className="text-2xl font-semibold">{organization.name}</h1>
+          <p>{organization.description}</p>
+        </div>
+      </OrganizationHeader>
+
+      <SingleOrganizationTournamentsTable
+        className="w-full px-4"
+        data={tournaments}
+        organization={organization}
+      />
+    </>
+  );
 }
