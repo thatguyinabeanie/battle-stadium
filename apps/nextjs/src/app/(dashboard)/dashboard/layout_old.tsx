0f87fdbacd8db4cc4411f2e20932864f129844d0
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-24 17:23:15 -0600

    layout old

---
+++ apps/nextjs/src/app/(dashboard)/dashboard/layout_old.tsx
@@ -0,0 +1,88 @@
+import { Suspense } from "react";
+
+import { TabsContent as UiTabsContent } from "@battle-stadium/ui";
+
+import type { DashboardLayoutSlots } from "~/types";
+import { getAccountMe } from "~/app/server-actions/accounts/actions";
+import { Tabs, TabsList, TabsTrigger } from "~/components/tabs/tabs";
+
+const tabsList = [
+  { key: "dashboard", title: "Dashboard" },
+  { key: "profiles", title: "Profiles" },
+  { key: "pokemon", title: "Pokemon" },
+  { key: "tournaments", title: "Tournaments" },
+  { key: "organizations", title: "Organizations" },
+  { key: "settings", title: "Settings" },
+];
+const adminTab = { key: "admin", title: "Admin" };
+
+export default function DashboardLayout(slots: Readonly<DashboardLayoutSlots>) {
+  return (
+    <div className="min-w-screen flex min-h-screen flex-col items-center pt-2">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Tabs defaultValue="dashboard" className="flex flex-col items-center">
+          <TabsList className="min-w-screen flex flex-row">
+            <TabsTriggers />
+          </TabsList>
+          <TabsContent {...slots} />
+        </Tabs>
+      </Suspense>
+    </div>
+  );
+}
+
+async function TabsContent(props: Readonly<DashboardLayoutSlots>) {
+  const me = await getAccountMe();
+  const tabsToRender = me?.admin ? [...tabsList, adminTab] : tabsList;
+
+  return (
+    <>
+      {tabsToRender.map(({ key }) => (
+        <UiTabsContent key={key} value={key} className="min-w-screen w-full">
+          {/* <div className="flex flex-col items-center"> */}
+          {renderTabContent(key, props)}
+          {/* </div> */}
+        </UiTabsContent>
+      ))}
+    </>
+  );
+}
+
+async function TabsTriggers() {
+  const me = await getAccountMe();
+  const tabsToRender = me?.admin ? [...tabsList, adminTab] : tabsList;
+
+  return (
+    <>
+      {tabsToRender.map(({ key, title }) => (
+        <TabsTrigger
+          key={key}
+          value={key}
+          title={title}
+          aria-label={`Switch to ${title} tab`}
+        />
+      ))}
+    </>
+  );
+}
+
+function renderTabContent(key: string, props: Readonly<DashboardLayoutSlots>) {
+  switch (key) {
+    case "profiles":
+      return props.profiles;
+    case "pokemon":
+      return props.pokemon;
+    case "tournaments":
+      return props.tournament_history;
+    case "dashboard":
+      return props.children;
+    case "settings":
+      return props.settings;
+    case "admin":
+      return props.admin;
+    case "organizations":
+      return props.organizations;
+    default:
+      return null;
+  }
+}
