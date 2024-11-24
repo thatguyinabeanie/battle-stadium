f26e90017236fad2b913d26f973e2e4236f31286
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-23 21:23:06 -0600

    bring page players/username

---
+++ apps/nextjs/src/app/players/[username]/page.tsx
@@ -0,0 +1,30 @@
+import { getProfile } from "~/app/server-actions/profiles/actions";
+import ComingSoon from "~/components/coming-soon";
+
+interface PlayerProfilePageProps {
+  params: Promise<{
+    username: string;
+  }>;
+}
+
+export async function generateMetadata (
+  props: Readonly<PlayerProfilePageProps>,
+) {
+  const params = await props.params;
+  const player = await getProfile(params.username);
+
+  return { title: player?.username ?? "Player" };
+}
+
+export default async function PlayerProfilePage (
+  props: Readonly<PlayerProfilePageProps>,
+) {
+  const params = await props.params;
+  const player = await getProfile(params.username);
+
+  return (
+    <ComingSoon title={ player?.username ?? "Player Profile" }>
+      <h2>The Player Profiles are under construction</h2>
+    </ComingSoon>
+  );
+}
