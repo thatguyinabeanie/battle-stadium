1200461607d16f5dea2ae8b497eb57ddca58949e
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-24 12:14:29 -0600

    added shadcnui sidebar

--- apps/nextjs/src/app/dashboard/@organizations/page.tsx
+++ apps/nextjs/src/app/dashboard/@organizations/page.tsx
@@ -1,11 +1,7 @@
-import { Card } from "@battle-stadium/ui";
-
 export default function OrganizationsDashboard() {
   return (
-    <div>
-    <Card className="flex flex-col h-screen w-screen ">
+    <>
 
-    </Card>
-    </div>
+    </>
   );
 }
