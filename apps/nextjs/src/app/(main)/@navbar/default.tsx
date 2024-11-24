deb0ffb4418fe7d2044e0a22e178c81b7d64d152
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-24 17:41:42 -0600

    move navbar components

--- apps/nextjs/src/app/(main)/@navbar/default.tsx
+++ apps/nextjs/src/app/(main)/@navbar/default.tsx
@@ -1,4 +1,4 @@
-import Navbar from "~/components/navbar/navbar";
+import Navbar from "~/app/(main)/@navbar/_navbar-components/navbar";
 
 export default function NavbarDefaultSlot() {
   return <Navbar />;
