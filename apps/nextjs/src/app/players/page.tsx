0691dbe47a575752dfbf8f7cdd1d52c086b9e210
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-23 21:24:23 -0600

    players table

---
+++ apps/nextjs/src/app/players/page.tsx
@@ -0,0 +1,11 @@
+import ComingSoon from "~/components/coming-soon";
+
+export default function PlayersPage () {
+  return (
+    <ComingSoon title={ "Player Profiles" }>
+      <div className="flex flex-col items-center">
+        <h2>Player profiles are under construction</h2>
+      </div>
+    </ComingSoon>
+  );
+}
