dd0cbbb260e81c46daaf2f1743d332cacb068c5d
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-23 21:12:08 -0600

    bring back register page

---
+++ apps/nextjs/src/app/organizations/[org_slug]/tournaments/[tournament_id]/register/page.tsx
@@ -0,0 +1,98 @@
+import { Input } from "@battle-stadium/ui";
+
+import type { OrganizationTournamentParams } from "~/types";
+import { getProfilesMe } from "~/app/server-actions/profiles/actions";
+import { postTournamentRegistration } from "~/app/server-actions/tournaments/actions";
+import { TournamentRegistrationForm } from "~/components/tournaments/tournament-registration";
+import { generateOrganizationTournamentsStaticParams } from "~/lib/organization-tournaments-static-params";
+
+export const revalidate = 300;
+export const dynamicParams = true;
+
+export async function generateStaticParams () {
+  return await generateOrganizationTournamentsStaticParams();
+}
+
+export default async function Register (
+  props: Readonly<OrganizationTournamentParams>,
+) {
+  const params = await props.params;
+  const { org_slug, tournament_id } = params;
+  const tournamentRegistration = tournamentRegistrationAction(tournament_id);
+
+  return (
+    <>
+      {/* <ToastContainer /> */ }
+      <div className="border-small m-20 inline-block max-w-fit justify-center rounded-3xl border-neutral-500/40 bg-transparent p-10 text-center backdrop-blur">
+        <div>
+          Register for { org_slug } tournament { tournament_id }
+        </div>
+
+        <TournamentRegistrationForm
+          { ...params }
+          tournamentRegistrationAction={ tournamentRegistration }
+        >
+          <Input name="ign" />
+          <ProfileSelector />
+        </TournamentRegistrationForm>
+      </div>
+    </>
+  );
+}
+
+function tournamentRegistrationAction (tournament_id: number) {
+  return async (formData: FormData) => {
+    "use server";
+    const profiles = await getProfilesMe();
+
+    const in_game_name = formData.get("ign") as string;
+    const profile = formData.get("profile") as string;
+    const show_country_flag =
+      (formData.get("country_flag") as string) === "true";
+
+    const profile_id = profiles.find((p) => p.username === profile)?.id;
+
+    if (!profile_id) {
+      throw new Error("Profile not found.");
+    }
+
+    return postTournamentRegistration({
+      tournamentId: tournament_id,
+      inGameName: in_game_name,
+      profileId: profile_id,
+      showCountryFlag: show_country_flag,
+    });
+  };
+}
+
+async function ProfileSelector () {
+  const profiles = await getProfilesMe();
+  return (
+    <>
+      <Input
+        type="text"
+        name="profile"
+        list="profiles"
+        placeholder="Select profile"
+        required
+        aria-label="Select your Profile"
+        aria-description="profile-description"
+      />
+
+      <div id="profile-description" className="sr-only">
+        Select your profile from the list of available profiles
+      </div>
+
+      <datalist id="profiles">
+        { profiles.map((profile) => (
+          <option
+            key={ profile.id }
+            value={ profile.username }
+            label={ profile.username }
+            data-profile-id={ profile.id }
+          />
+        )) }
+      </datalist>
+    </>
+  );
+}
