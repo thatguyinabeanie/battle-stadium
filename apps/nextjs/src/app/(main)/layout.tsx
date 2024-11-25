e0c37086cd91e36109e30523b17adc48dab48ae3
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-24 18:34:30 -0600

    scalar

--- apps/nextjs/src/app/(main)/layout.tsx
+++ apps/nextjs/src/app/(main)/layout.tsx
@@ -40,6 +40,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   minimumScale: 1,
+  userScalable: true,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },
