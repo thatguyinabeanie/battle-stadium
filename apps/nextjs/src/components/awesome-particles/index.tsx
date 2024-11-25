dd8c2d118ef918e61ea9375d2d5ae6fb45f0b725
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-24 20:15:47 -0600

    seems to be stable

--- apps/nextjs/src/components/awesome-particles/index.tsx
+++ apps/nextjs/src/components/awesome-particles/index.tsx
@@ -3,9 +3,8 @@
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 // import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-
-// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 // import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 
 import useParticleOptions from "./use-particle-options";
@@ -17,7 +16,7 @@ export default function AwesomeParticles() {
 
   useEffect(() => {
     try {
-      initParticlesEngine(loadFull)
+      initParticlesEngine(loadSlim)
         .then(() => setInit(true))
         .catch((error) => {
           console.log("error", error);
