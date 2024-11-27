b1caf86a8a0ca1664c6717b5f0cc4691ec74d229
Gabe Mendoza <thatguyin@beanie.gg>
2024-11-27 00:55:12 -0600

    Refactor sidebar components and improve linting (#70)
    * lint
    
    * remove console.log
    
    * animate pulse
    
    * Update apps/nextjs/src/app/(dashboard)/dashboard/_components/sidebar/dropdown-menu-content-mobile.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Gabe Mendoza <thatguyin@beanie.gg>
    
    * Update apps/nextjs/src/app/(dashboard)/dashboard/_components/sidebar/nav-menu-switcher.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: Gabe Mendoza <thatguyin@beanie.gg>
    
    ---------
    
    Signed-off-by: Gabe Mendoza <thatguyin@beanie.gg>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ apps/nextjs/src/app/(dashboard)/dashboard/_components/sidebar/nav-menu-switcher.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import Link from "next/link";
+import { ChevronsUpDown } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@battle-stadium/ui";
+
+import { BattleStadiumLogoLink } from "~/components/battle-stadium";
+import { NavbarItemsConfigs } from "~/lib/config/site";
+import { DropDownMenuContentMobile } from "./dropdown-menu-content-mobile";
+
+export function MainAppLinksMenu() {
+  return (
+    <div className="flex flex-row items-center justify-center gap-2">
+      <div className="flex aspect-square size-8 items-center justify-center rounded-lg text-sidebar-primary-foreground">
+        <BattleStadiumLogoLink />
+      </div>
+
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <SidebarMenuButton
+                size="lg"
+                aria-label="Menu with links to main app"
+                role="button"
+                aria-haspopup="true"
+                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                <>
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-semibold">
+                      Battle Stadium
+                    </span>
+                    <span className="truncate text-xs">Beanie LLC</span>
+                  </div>
+                </>
+
+                <ChevronsUpDown className="ml-auto" />
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+
+            <DropDownMenuContentMobile>
+              <DropdownMenuLabel className="text-xs text-muted-foreground">
+                Teams
+              </DropdownMenuLabel>
+
+              {NavbarItemsConfigs.map((navItem, index) => (
+                <Link key={navItem.value} href={`/${navItem.value}`}>
+                  <DropdownMenuItem className="gap-2 p-2">
+                    <div className="flex size-6 items-center justify-center rounded-sm border">
+                      <navItem.logo className="size-4 shrink-0" />
+                    </div>
+                    {navItem.label}
+                    <DropdownMenuShortcut>⌘{index + 1}</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </Link>
+              ))}
+            </DropDownMenuContentMobile>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </div>
+  );
+}
--- apps/nextjs/src/app/(dashboard)/dashboard/_components/sidebar/nav-user.tsx
+++ apps/nextjs/src/app/(dashboard)/dashboard/_components/sidebar/nav-user.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import {
   BadgeCheck,
   Bell,
@@ -10,11 +8,7 @@ import {
 } from "lucide-react";
 
 import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
   DropdownMenu,
-  DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
@@ -23,20 +17,12 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@battle-stadium/ui";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
-  const { isMobile } = useSidebar();
+import { DropDownMenuContentMobile } from "./dropdown-menu-content-mobile";
+import { SidebarNavUserDetailsAndAvatarSuspense } from "./sidebar-navuser-avatar-details";
 
+export function NavUserComponent() {
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -44,68 +30,56 @@ export function NavUser({
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
               size="lg"
-              aria-label={`User menu for ${user.name}`}
+              aria-label={"User menu"}
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
+              <SidebarNavUserDetailsAndAvatarSuspense />
               <ChevronsUpDown className="ml-auto size-4" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
-                </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <LogOut />
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
+
+          <NavUserDropdownMenuContent />
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
+  );
+}
+
+function NavUserDropdownMenuContent() {
+  return (
+    <DropDownMenuContentMobile>
+      <DropdownMenuLabel className="p-0 font-normal">
+        <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+          <SidebarNavUserDetailsAndAvatarSuspense />
+        </div>
+      </DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <Sparkles />
+          Upgrade to Pro
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          <BadgeCheck />
+          Account
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <CreditCard />
+          Billing
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Bell />
+          Notifications
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem>
+        <LogOut />
+        Log out
+      </DropdownMenuItem>
+    </DropDownMenuContentMobile>
   );
 }
