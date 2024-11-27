d98ad4b83f696997dc634cbb73380d3b68132a85
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-26 20:17:31 -0600

    update classnames

--- apps/nextjs/src/app/(main)/@navbar/_navbar-components/navbar-links.tsx
+++ apps/nextjs/src/app/(main)/@navbar/_navbar-components/navbar-links.tsx
@@ -31,7 +31,7 @@ async function DashboardNavLink() {
       value="dashboard"
       key="dashboard"
       href="/dashboard"
-      className={ cn("hidden", { "sm:flex": !sessionId})}
+      className={ cn("items-center", sessionId ? "md:flex" : "hidden") }
       label="Dashboard"
     />
   );
