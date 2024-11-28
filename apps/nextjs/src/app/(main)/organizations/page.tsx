b88c39b44d47d3e91d999b8a678f6a3e16f8e784
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-28 00:46:00 -0600

    org grid

--- apps/nextjs/src/app/(main)/organizations/page.tsx
+++ apps/nextjs/src/app/(main)/organizations/page.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
 
+import type { Organization } from "@battle-stadium/db/schema";
+import { Card, CardFooter } from "@battle-stadium/ui";
+
 import { getOrganizations } from "~/app/server-actions/organizations/actions";
-import OrganizationsTable from "~/components/organizations/organizations-table";
+import OrganizationLogo from "~/components/organizations/organization-logo";
 import PartneredOrganizations from "~/components/organizations/partnered-organizations";
 
 export const metadata: Metadata = {
@@ -13,21 +16,36 @@ export default function OrganizationsPage() {
   return (
     <>
       <PartneredOrganizations />
-      <OrganizationsTableSuspenseWrapper />
+      <Suspense fallback={<div>Loading organizations...</div>}>
+        <OrganizationsGridRSC />
+      </Suspense>
     </>
   );
 }
 
-function OrganizationsTableSuspenseWrapper() {
-  // TODO: Implement a proper skeleton for OrganizationsTable
+async function OrganizationsGridRSC() {
+  const orgs = await getOrganizations();
   return (
-    <Suspense fallback={<div>Loading organizations...</div>}>
-      <OrganizationsTableServerComponent />
-    </Suspense>
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <div className="grid auto-rows-min grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {orgs.map((org) => (
+          <SimpleOrgCard key={org.id} org={org} />
+        ))}
+      </div>
+    </div>
   );
 }
 
-async function OrganizationsTableServerComponent() {
-  const orgs = await getOrganizations();
-  return <OrganizationsTable orgs={orgs} />;
+function SimpleOrgCard({ org }: { org: Organization }) {
+  return (
+    <Card
+      key={org.id}
+      className="flex aspect-square h-44 flex-col items-center justify-around rounded-xl bg-muted/50 md:h-60"
+    >
+      <OrganizationLogo organization={org} logoSize={175} />
+      <CardFooter className="text-center text-lg font-bold">
+        {org.name}
+      </CardFooter>
+    </Card>
+  );
 }
