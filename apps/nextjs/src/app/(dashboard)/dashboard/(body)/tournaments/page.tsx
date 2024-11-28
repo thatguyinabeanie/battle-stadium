9d2c9f7d4c509a1eab7f9c6cab78f2a45cbecf60
thatguyinabeanie <thatguyin@beanie.gg>
2024-11-27 22:56:59 -0600

    section

--- apps/nextjs/src/app/(dashboard)/dashboard/(body)/tournaments/page.tsx
+++ apps/nextjs/src/app/(dashboard)/dashboard/(body)/tournaments/page.tsx
@@ -4,15 +4,14 @@ export default function TournamentHistory() {
   return (
     <div className="m-4 flex flex-col px-4 py-8 sm:px-8 md:px-16 lg:px-80">
       <ComingSoon title="Tournament History">
-        <div
+        <section
           className="flex flex-col items-center"
-          role="status"
           aria-label="Construction Status"
         >
           <h2 className="text-xl font-semibold">
             Tournament History is Under Construction
           </h2>
-        </div>
+        </section>
       </ComingSoon>
     </div>
   );
