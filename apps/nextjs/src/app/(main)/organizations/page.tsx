3e49e3c00dc15105bd9e5f0d9f95deb3372c379b
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-01 15:54:24 -0600

    orgs index styling

--- apps/nextjs/src/app/(main)/organizations/page.tsx
+++ apps/nextjs/src/app/(main)/organizations/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { memo, Suspense } from "react";
+import Link from "next/link";
 
 import type { Organization } from "@battle-stadium/db/schema";
 import { Card, CardFooter } from "@battle-stadium/ui";
@@ -42,11 +43,17 @@ async function OrganizationsGridRSC() {
 
 function SimpleOrgCard({ org }: { org: Organization }) {
   return (
-    <Card className="flex aspect-square h-44 flex-col items-center justify-around rounded-xl bg-muted/50 md:h-60">
-      <OrganizationLogo organization={org} logoSize={140} />
-      <CardFooter className="text-center text-lg font-bold">
-        {org.name}
-      </CardFooter>
-    </Card>
+    <Link href={`/organizations/${org.slug}`}>
+      <Card className="flex aspect-square h-44 flex-col items-center justify-around rounded-xl bg-muted/50 md:h-60">
+        <OrganizationLogo
+          organization={org}
+          logoSize={140}
+          className="hover:z-50 hover:scale-105"
+        />
+        <CardFooter className="text-center text-lg font-bold text-primary hover:z-50 hover:scale-105">
+          {org.name}
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
