073e754a8bb25c505b09689907621167a48f0eda
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-02 11:32:27 -0600

    clerk auth simplify

---
+++ apps/nextjs/src/app/(main)/(sign-in-sign-up)/_components/clerk-auth.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useUser, SignIn , SignUp} from "@clerk/nextjs";
+
+interface ClerkAuthProps {
+  type: "sign-in" | "sign-up";
+}
+
+export function ClerkAuth ({type}: ClerkAuthProps) {
+  const { user } = useUser();
+
+  if (user) {
+    return <div>You are signed in.. </div>;
+  }
+
+  if (type === "sign-in") {
+    return <SignIn />;
+  }
+
+  return <SignUp />;
+}
