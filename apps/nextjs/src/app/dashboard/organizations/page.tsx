811500ccfa6dc600803d66a4c3bd62f6faae3ee3
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-03 15:33:01 -0600

    lint

--- apps/nextjs/src/app/dashboard/organizations/page.tsx
+++ apps/nextjs/src/app/dashboard/organizations/page.tsx
@@ -1,3 +1,16 @@
-export default function DashboardProfiles() {
-  return <h1>Dashboard Organization Page</h1>;
+import ComingSoon from "~/components/coming-soon";
+
+export default function DashboardDefault() {
+  return (
+    <div className="m-4 flex flex-col px-4 py-8 sm:px-8 md:px-16 lg:px-80">
+      <ComingSoon title="Organizations Dashboard Home">
+        <section
+          className="flex flex-col items-center"
+          aria-label="Construction Status"
+        >
+          <h1 className="text-xl font-semibold">Under construction</h1>
+        </section>
+      </ComingSoon>
+    </div>
+  );
 }
