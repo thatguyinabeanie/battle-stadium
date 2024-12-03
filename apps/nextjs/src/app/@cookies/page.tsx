b9f842e5281c1222d5cc7f6ea989aa7f57a93bd5
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-03 11:47:01 -0600

    lint

--- apps/nextjs/src/app/@cookies/page.tsx
+++ apps/nextjs/src/app/@cookies/page.tsx
@@ -1,11 +1,5 @@
-import { Suspense } from "react";
-
 import CookiesServerComponent from "./cookies-server-component";
 
 export default function CookiesSlot() {
-  return (
-    <Suspense fallback={null}>
-      <CookiesServerComponent />
-    </Suspense>
-  );
+  return <CookiesServerComponent />;
 }
