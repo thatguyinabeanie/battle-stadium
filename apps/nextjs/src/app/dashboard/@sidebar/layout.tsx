8a6dc02deeac5c4940109b40221fe1fac1410049
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-03 16:41:00 -0600

    Flat Sidebar Slot

--- apps/nextjs/src/app/dashboard/@sidebar/layout.tsx
+++ apps/nextjs/src/app/dashboard/@sidebar/layout.tsx
@@ -4,6 +4,9 @@ import {
   SidebarFooter,
   SidebarHeader,
 } from "@battle-stadium/ui/sidebar";
+import Header from "./header";
+import Footer from "./footer";
+import { Suspense } from "react";
 
 interface SideBarSlots {
   sidebarheader: React.ReactNode;
@@ -14,19 +17,23 @@ interface SideBarSlots {
 const SIDEBAR_DEFAULT_CLASSNAMES = "m-4 rounded-xl bg-neutral-900/50";
 export default function SideBar({
   children,
-  sidebarfooter,
-  sidebarheader,
 }: SideBarSlots) {
   return (
     <Sidebar variant="sidebar" collapsible="icon">
       <SidebarHeader className={SIDEBAR_DEFAULT_CLASSNAMES}>
-        {sidebarheader}
+        <Suspense fallback={null}>
+          <Header />
+        </Suspense>
       </SidebarHeader>
+
       <SidebarContent className={cn(SIDEBAR_DEFAULT_CLASSNAMES, "mt-0")}>
         {children}
       </SidebarContent>
+
       <SidebarFooter className={cn(SIDEBAR_DEFAULT_CLASSNAMES, "mt-0")}>
-        {sidebarfooter}
+        <Suspense fallback={ null }>
+          <Footer />
+        </Suspense>
       </SidebarFooter>
     </Sidebar>
   );
