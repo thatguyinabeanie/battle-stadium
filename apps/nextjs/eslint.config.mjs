a11a854fb8c90f5fcb5938687d61191cf8b37920
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-02 18:31:29 -0600

    save

--- apps/nextjs/eslint.config.mjs
+++ apps/nextjs/eslint.config.mjs
@@ -1,5 +1,3 @@
-import nextPlugin from "@next/eslint-plugin-next";
-
 import baseConfig, { restrictEnvAccess } from "@battle-stadium/eslint-config/base";
 import nextjsConfig from "@battle-stadium/eslint-config/nextjs";
 import reactConfig from "@battle-stadium/eslint-config/react";
---
+++ tooling/eslint/react.js
@@ -0,0 +1,30 @@
+import reactPlugin from "eslint-plugin-react";
+import reactCompilerPlugin from "eslint-plugin-react-compiler";
+import hooksPlugin from "eslint-plugin-react-hooks";
+
+/** @type {Awaited<import('typescript-eslint').Config>} */
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": hooksPlugin,
+      "react-compiler": reactCompilerPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      "react-compiler/react-compiler": "error",
+    },
+    languageOptions: {
+      globals: {
+        React: "writable",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
