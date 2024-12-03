46e5cb9835e0485e415fd29b071d448faf8bb8fb
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-03 15:22:35 -0600

    lint

--- apps/nextjs/src/app/dashboard/@sidebar/page.tsx
+++ apps/nextjs/src/app/dashboard/@sidebar/page.tsx
@@ -6,10 +6,8 @@ import { NavMain } from "./_components/nav-main/nav-main";
 const SIDEBAR_DEFAULT_CLASSNAMES = "m-4 rounded-xl bg-neutral-900/50";
 export default function DashboardSideBar() {
   return (
-    <div className="flex h-full flex-col">
-      <SidebarContent className={cn(SIDEBAR_DEFAULT_CLASSNAMES, "mt-0")}>
-        <NavMain />
-      </SidebarContent>
-    </div>
+    <SidebarContent className={cn(SIDEBAR_DEFAULT_CLASSNAMES, "mt-0")}>
+      <NavMain />
+    </SidebarContent>
   );
 }
