3296be572d800b2008e6a45b1fdfbd02c84fedb5
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-03 16:21:09 -0600

    lint

--- apps/nextjs/src/app/dashboard/loading.tsx
+++ apps/nextjs/src/app/dashboard/loading.tsx
@@ -1,3 +1,17 @@
-import DashboardContentSkeleton from "~/app/dashboard/_components/dashboard-content-skeleton";
+import { DashboardContentSection } from "./_components/dashboard-content-section";
 
-export default DashboardContentSkeleton;
+export default function Loading() {
+  return (
+    <>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <DashboardContentSection />
+        <DashboardContentSection />
+        <DashboardContentSection />
+      </div>
+      <main className="min-h-[100vh] flex-1 animate-pulse rounded-xl bg-neutral-900/50 md:min-h-min" />
+      <div className="sr-only" aria-live="polite">
+        Loading dashboard content, please wait...
+      </div>
+    </>
+  );
+}
