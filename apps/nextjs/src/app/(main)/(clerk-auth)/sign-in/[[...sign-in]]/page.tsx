1961656e2f20d96499c6d75fbe2f66b9c85422c8
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-02 23:03:42 -0600

    refactor: streamline sign-in and sign-up page structure by removing unnecessary Suspense components

--- apps/nextjs/src/app/(main)/(clerk-auth)/sign-in/[[...sign-in]]/page.tsx
+++ apps/nextjs/src/app/(main)/(clerk-auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,11 @@
-import { Suspense } from "react";
 import { SignedOut, SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <SignedOut>
-      <div className="flex h-full min-h-screen w-full items-center justify-center">
-        <Suspense fallback={<div>Loading...</div>}>
-          <SignIn />
-        </Suspense>
-      </div>
-    </SignedOut>
+    <div className="flex h-full min-h-screen w-full items-center justify-center">
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
+    </div>
   );
 }
--- apps/nextjs/src/app/(main)/(clerk-auth)/sign-up/[[...sign-up]]/page.tsx
+++ apps/nextjs/src/app/(main)/(clerk-auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,14 +1,11 @@
-import { Suspense } from "react";
 import { SignedOut, SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
-    <SignedOut>
-      <div className="flex h-full min-h-screen w-full items-center justify-center">
-        <Suspense fallback={<div>Loading...</div>}>
-          <SignUp />
-        </Suspense>
-      </div>
-    </SignedOut>
+    <div className="flex h-full min-h-screen w-full items-center justify-center">
+      <SignedOut>
+        <SignUp />
+      </SignedOut>
+    </div>
   );
 }
