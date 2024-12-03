6a280f7707e0bdd1adf26849de0e5a802e7631ac
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-03 15:43:51 -0600

    delete extraneous files

--- apps/nextjs/src/app/dashboard/@sidebar/@sidebarfooter/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NavUserComponent } from "../_components/nav-user";
-
-export default function SidebarFooterSlot() {
-  return <NavUserComponent />;
-}
--- apps/nextjs/src/app/dashboard/@sidebar/@sidebarheader/page.tsx
+++
@@ -1,5 +0,0 @@
-import { MainAppLinksMenu } from "./links-menu";
-
-export default function SidebarHeaderSlot() {
-  return <MainAppLinksMenu />;
-}
