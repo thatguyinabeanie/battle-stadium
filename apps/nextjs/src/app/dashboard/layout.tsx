8185d59564682d063c28c94d44b35aef22173d1c
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-04 11:24:37 -0600

    styling

--- apps/nextjs/src/app/dashboard/layout.tsx
+++ apps/nextjs/src/app/dashboard/layout.tsx
@@ -38,29 +38,33 @@ export default function DashboardLayout({
 }: Readonly<DashboardLayoutProps>) {
   return (
     <BreadCrumbsProvider>
-      <SidebarProvider className="border-[1px]">
+      <SidebarProvider className="border-none">
         <SideBar>{sidebar}</SideBar>
-
         <SidebarInset className="border-none bg-transparent">
-          <header className={HEADER_CLASSES.CONTAINER}>
-            <div className={HEADER_CLASSES.INNER}>
-              <SidebarTrigger className="ml-1" aria-label="Open Sidebar" />
-              <Separator
-                orientation="vertical"
-                className={`mr-2 ${LAYOUT_CONSTANTS.SEPARATOR_HEIGHT}`}
-              />
-              {breadcrumbs}
-            </div>
-          </header>
-
+          <BreadCrumbsContainer>{breadcrumbs}</BreadCrumbsContainer>
           <div
             aria-label="dashboard content"
-            className="my-4 mr-4 flex min-h-[100dvh] flex-1 flex-col gap-4 rounded-xl bg-neutral-900/50 md:min-h-min"
+            className="my-4 mr-4 flex flex-1 flex-col gap-4 rounded-xl bg-neutral-900/50"
           >
             {children}
           </div>
         </SidebarInset>
       </SidebarProvider>
     </BreadCrumbsProvider>
+  );
+}
+
+function BreadCrumbsContainer({ children }: ChildrenProps) {
+  return (
+    <header className={HEADER_CLASSES.CONTAINER}>
+      <div className={HEADER_CLASSES.INNER}>
+        <SidebarTrigger className="ml-1" aria-label="Open Sidebar" />
+        <Separator
+          orientation="vertical"
+          className={`mr-2 ${LAYOUT_CONSTANTS.SEPARATOR_HEIGHT}`}
+        />
+        {children}
+      </div>
+    </header>
   );
 }
