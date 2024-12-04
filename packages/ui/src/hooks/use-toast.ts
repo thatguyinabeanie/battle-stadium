df2291badb65bc17fc4960d88bd41a0c60749e40
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-04 10:33:37 -0600

    genId

--- packages/ui/src/hooks/use-toast.ts
+++ packages/ui/src/hooks/use-toast.ts
@@ -22,11 +22,8 @@ export const actionTypes = {
   REMOVE_TOAST: "REMOVE_TOAST",
 } as const;
 
-let count = 0;
-
 function genId() {
-  count = (count + 1) % Number.MAX_SAFE_INTEGER;
-  return count.toString();
+  return `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
 }
 
 type ActionType = typeof actionTypes;
