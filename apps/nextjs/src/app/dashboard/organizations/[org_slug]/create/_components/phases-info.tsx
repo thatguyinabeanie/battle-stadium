47197eab1ccaf88759393f0bd6aacf81b14abaca
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-06 16:44:30 -0600

    Number.parseInt

--- apps/nextjs/src/app/dashboard/organizations/[org_slug]/create/_components/phases-info.tsx
+++ apps/nextjs/src/app/dashboard/organizations/[org_slug]/create/_components/phases-info.tsx
@@ -157,7 +157,7 @@ function BestOfSelect({ index, phase, setPhaseKeyValue }: PhaseFormProps) {
           { value: "three", label: "3" },
         ]}
         value={String(phase.bestOf)}
-        onValueChange={(value) => setPhaseKeyValue(index, "bestOf")(parseInt(value, 10))}
+        onValueChange={(value) => setPhaseKeyValue(index, "bestOf")(Number.parseInt(value, 10))}
       />
     </InputWrapper>
   );
