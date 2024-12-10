1b40166da2f7e4ee439e23216737973a4900f184
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-09 23:50:40 -0600

    save

--- apps/nextjs/src/app/(main)/players/[username]/page.tsx
+++ apps/nextjs/src/app/(main)/players/[username]/page.tsx
@@ -28,7 +28,7 @@ async function PlayerProfile({ username }: ProfilePageProps) {
   const profile_username = (await getProfile(username))?.username ?? "Player";
   return (
     <section
-      aria-label="Tournament Header"
+      aria-label="Player Profile Content"
       className="z-0 m-4 mt-0 flex h-full w-full flex-col items-center gap-4 rounded-xl bg-neutral-950"
     >
       <ComingSoon title={`${profile_username} Profile`} />
