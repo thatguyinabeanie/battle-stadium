adff132a4bff1cd2075faeb78317990e5569c131
thatguyinabeanie <thatguyin@beanie.gg>
2024-12-15 12:43:19 -0600

    save clerk pages

--- apps/nextjs/src/app/(main)/(clerk-auth)/sign-up/[[...sign-up]]/page.tsx
+++ apps/nextjs/src/app/(main)/(clerk-auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,21 +1,20 @@
-"use cache";
 import { Suspense } from "react";
 import { SignedOut, SignUp } from "@clerk/nextjs";
 import { ErrorBoundary } from "~/components/error-boundary";
 
 export default function SignUpPage() {
   return (
-    <section
-      aria-label="Sign Up Page"
-      className="z-0 m-4 mt-0 flex h-svh w-full flex-col items-center justify-around gap-4 rounded-xl bg-neutral-950"
-    >
-      <SignedOut>
-        <ErrorBoundary>
-          <Suspense fallback={<div>Loading...</div>}>
-            <SignUp />
-          </Suspense>
-        </ErrorBoundary>
-      </SignedOut>
-    </section>
+    <ErrorBoundary>
+      <section
+        aria-label="Sign Up Page"
+        className="z-0 m-4 mt-0 flex h-svh w-full flex-col items-center justify-around gap-4 rounded-xl bg-neutral-950"
+      >
+        <SignedOut>
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignUp />
+            </Suspense>
+        </SignedOut>
+      </section>
+    </ErrorBoundary>
   );
 }
