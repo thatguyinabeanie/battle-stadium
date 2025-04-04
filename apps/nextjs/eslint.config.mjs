9fabf68882608ed9c0d607b388d48dc3f28b2043
Gabe Mendoza <gmendoza@civisanalytics.com>
2025-04-04 16:42:45 -0500

    save. shrug. i give up

--- apps/nextjs/eslint.config.mjs
+++ apps/nextjs/eslint.config.mjs
@@ -1,6 +1,5 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import js from "@eslint/js";
 
 import nextPlugin from "@next/eslint-plugin-next";
 import baseConfig from "@battle-stadium/eslint-config/base";
@@ -11,17 +10,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const eslintConfig = [
-  js.configs.recommended,
   ...baseConfig,
   ...restrictEnvAccess,
   {
     files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
     plugins: {
       "@next/next": nextPlugin
-    },
-    rules: {
-      ...nextPlugin.configs["recommended"].rules,
-      "@next/next/no-html-link-for-pages": "error",
     },
     settings: {
       next: {
